74d113efcc05f66f7360b1d44dda349235ad6583
Ravi Tiwari <raviashoktiwari9559@gmail.com>
2026-03-01 18:38:35 +0530

    updating dbConnectjs

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -2,9 +2,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv"
 
 dotenv.config()
+let cached =null;
 export const dbConnect =async()=>{
     try {
-        await mongoose.connect(process.env.MONGODB_URL);//here we connect the database
+        if(!cached){
+            cached = await mongoose.connect(process.env.MONGODB_URL);//here we connect the database
+
+        }
         console.log('Database connected successfully..');
     } catch (error) {
         console.log("Database connection failed..",error)
